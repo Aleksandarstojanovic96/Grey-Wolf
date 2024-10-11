f1c979226f0ea093ea76092d34a87e379121e7b1
Aleksandarstojanovic96 <31633804+Aleksandarstojanovic96@users.noreply.github.com>
2024-10-11 14:13:09 +0200

    Add files via upload

--- send_email.php
+++ send_email.php
@@ -1,20 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitizing input data
     $name = htmlspecialchars(trim($_POST["name"]));
     $email = htmlspecialchars(trim($_POST["email"]));
     $message = htmlspecialchars(trim($_POST["message"]));
-    // Email Configuration
-    $to = "stojanaleksandrovic@gmail.com";
-    $subject = "New Contact Form Submission from $name"; 
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
-    $headers = "From: $email";
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Email sent successfully!";
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    // Verify reCAPTCHA
+    $secretKey = '6LdnSl0qAAAAACsXUtTsRi_E8t-QSh6tt5qB5DVa';
+    $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$recaptchaResponse}");
+    $responseData = json_decode($verifyResponse);
+    if ($responseData->success) {
+        // Email Configuration
+        $to = "stojanaleksandrovic@gmail.com";
+        $subject = "New Contact Form Submission from $name";
+        $body = "Name: $name\nEmail: $email\nMessage: $message";
+        $headers = "From: $email";
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Email sent successfully!";
+        } else {
+            echo "Failed to send email.";
+        }
     } else {
-        echo "Failed to send email.";
+        echo "reCAPTCHA verification failed. Please try again.";
     }
 } else {
-    header("index.html");
+    header("HTTP/1.1 405 Method Not Allowed");
+    echo "Method not allowed.";
     exit();
 }
 ?>
